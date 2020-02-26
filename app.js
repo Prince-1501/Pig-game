3d8c18dc7fe0d88e44cdb2eda4f536ca56acca27
Prince-1501 <princekumarrpcsvm10@gmail.com>
2020-02-26 16:18:22 +0530

    change the winning score

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
 
         //3. Chekc if player Won the Game
-        if(scores[activePlayer]>=20){
+        if(scores[activePlayer]>=100){
             document.querySelector('#name-'+ activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
